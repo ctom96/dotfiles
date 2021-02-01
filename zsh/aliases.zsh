2782e408801880ce197fca8821d5684be69be0c4
Chris Tomasello <ctom96@vt.edu>
2021-01-31 20:29:36 -0500

    Added path

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -1,8 +1,19 @@
+# neovim as default
+alias vim="nvim"
+
 # Git stuff
 alias gits="git status"
 alias gita="git add -A"
 alias gitc="git commit -a"
 alias gitp="git push"
+alias gitpo="git push origin master"
+alias gitnb="git checkout -b"
+alias gitdb="git branch -d"
+alias gitDb="git branch -D"
 
-# neovim default
-alias vim="nvim"
+# Python aliases
+alias python="python3.8"
+alias pip="python -m pip"
+
+# Random
+alias open="xdg-open"
---
+++ zsh/path.zsh
@@ -0,0 +1,11 @@
+#
+# PATH exports
+#
+
+export PATH="$PATH:~/.yarn/bin"
+export PATH="$PATH:/usr/local/go/bin"
+
+# Golang path stuff
+export GOPATH="/home/ctom96/go"
+export GOBIN=$GOPATH/bin
+export PATH=$PATH:$GOBIN
